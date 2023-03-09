19e3be4d6d31cd8a266069c94b59080bd377589e
Abijahebe <Abijahebe911@gmail.com>
2023-03-09 21:58:15 +0000

    correct

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    from sys import argv, exit
+from sys import argv
+from calculator_1 import add, sub, mul, div
 
-    if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+if __name__ == '__main__':
+    l_av = len(argv) - 1
+
+    if l_av == 3:
+        operator = argv[2]
+        num_a = int(argv[1])
+        num_b = int(argv[3])
+        if operator == '+':
+            res = add(num_a, num_b)
+            print('{:d} + {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        elif operator == '-':
+            res = sub(num_a, num_b)
+            print('{:d} - {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        elif operator == '*':
+            res = mul(num_a, num_b)
+            print('{:d} * {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        elif operator == '/':
+            res = div(num_a, num_b)
+            print('{:d} / {:d} = {:d}'.format(num_a, num_b, res))
+            exit(0)
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
+    else:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-    op = argv[2]
-    if op is not "+" and op is not "-" and \
-       op is not "*" and op is not "/":
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    if op is "+":
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op is "-":
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op is "*":
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op is "/":
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
