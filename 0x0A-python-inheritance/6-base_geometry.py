5867d3c49bcfbf3557bd25113b66897eb3a5fec7
Abijahebe <Abijahebe911@gmail.com>
2023-04-12 11:18:44 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""contains a class that defines BaseGeometry"""
+
+
+class BaseGeometry: """A class for basic geometric shapes"""
+
+def __init__(self):
+    pass
+
+def area(self):
+    """Raises an Exception with the message area() is not implemented"""
+    raise Exception("area() is not implemented")
