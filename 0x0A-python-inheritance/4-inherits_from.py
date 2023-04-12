efbcd4072f34937fe4d1d16adbcd0225ca0867c3
Abijahebe <Abijahebe911@gmail.com>
2023-04-12 10:41:56 +0000

    tasks 3 & 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""contains a func that determines if an objs class inherits a class"""
+
+
+def inherits_from(obj, a_clas):
+	"""determines if an objs class inherits from a class"""
+	cls = type(obj)
+	if cls is a_class:
+		return False
+
+	return issubclass(cls, a_class)
