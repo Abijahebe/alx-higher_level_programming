b2cfc8ff038f4411598e32180c67877df5f1b0e4
Abijahebe <Abijahebe911@gmail.com>
2023-04-15 14:51:07 +0000

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,15 +5,6 @@
 
 
 def write_file(filename="", text=""):
-	"""writes a string to a text file (UTF8)
-
-	Args:
-		filename: name of the file
-		text: string to write file
-
-	Return:
-		number of characters written to file
-	"""
-	if filename:
-	with open(filename, 'w', encoding='utf-8') as writeFile:
-		return writeFile.write(text)
+	with open(filename, 'w', encoding='utf-8') as file:
+		file.write(text)
+		return len(text)
