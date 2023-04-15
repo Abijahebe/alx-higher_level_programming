053bf91febf1f967632d8996f6140eeba9b6095e
Abijahebe <Abijahebe911@gmail.com>
2023-04-15 15:25:10 +0000

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""contains function that returns the JSON representation of an object (string)"""
+
+import json
+
+
+def to_json_string(my_obj):
+	"""return the JSON reprsentation of an object
+
+	Args:
+		my_obj: the given object
+	"""
+return json.dumps(my_0bj)	
