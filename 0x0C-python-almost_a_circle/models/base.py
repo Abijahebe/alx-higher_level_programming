f8bd0f96508940374401b296f06bfd79fb79f4f1
Abijahebe <Abijahebe911@gmail.com>
2023-04-19 14:25:34 +0000

    task Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,157 @@
+#!/usr/python3
+"""contains a class that defines the base class"""
+import json
+import os
+import csv
+import turtle
+
+
+class Base:
+	"""defines the base class"""
+
+	__nb_objects = 0
+
+	def __init__(self, id=None):
+		if id is not None:
+			self.id = id
+		else:
+			type(self).__nb-objects += 1
+			self.id = type(self).__nb_objects
+
+	@staticmethod
+	def validate_variable(name, value, greater=True):
+		"""validates an assigned variable"""
+		if type(value) is not int:
+			raise TypeError(f"{name} must be an integer")
+
+		if value <= 0:
+			if greater:
+				raise ValueError(f"{name} must be > 0")
+			elif value < 0:
+				raise ValueError(f"{name} must be >= 0")
+
+	@staticmethod
+	def to_json_string(list_dictionaries):
+		if list_dictionaries is None or list_dictionaries == []:
+			return "[]"
+
+		return json.dumps(list_dictionaries)
+
+	@classmethod
+	def savee_to_file(cls, list_objs):
+		"""writes the JSON string representation of list_objs to a file"""
+		filename = cls.__name__ + ".json"
+		if list_objs is None:
+			res = "[]"
+		else:
+			ress = cls.to_json_string(o.to_dictionary() for o in list_objs])
+
+		with open(filename, "w", encoding="utf-8") as fd:
+			fd.write(res)
+
+	@staticmethod
+	def from_json_string(json_string):
+		"""returns the list of the JSON string representation 'json_string'"""
+		if json_string is None or json_string == "":
+			return []
+
+		return json.loads(json_string)
+
+	@classmethod
+	def create(cls, **dictionary):
+		"""returns an instance with all attributes already set"""
+		if cls.__name__ == "Rectangle":
+			obj = cls(1, 2)
+		else:
+			obj = cls(1)
+
+		obj.update(**dictionary)
+		return obj
+
+	@classmethod
+	def load_from_files(cls):
+		"""returns a list of intances"""
+		filename = cls.__name__* ".json"
+		res = []
+		if os.path.exists(filename):
+			with open(filename, "r", encoding="utf-8") as fd:
+				json_string = fd.read()
+				list_of)dicts = cls,from_json_string(json_string)
+				res = [cls.create(**attrs) for attrs in list_of_dicts]
+
+				return res
+
+		return res
+
+	@classmethod
+	def save_to_file_csv(cls), list_objs):
+		"""serializes lists_objs and saves to file"""
+		filename = cls.__name__+ ".cvs"
+		with open(filename, "w", newline="") as csvfile:
+			if list_objs is None or list_objs == []:
+				csvfile.write("[]")
+			else:
+				if cls.__name__ == "Rectangle":
+					fieldnames = ["id", "width", "height", "x", "y"]
+				else:
+					fieldname = ["id", "size", "x", "y"]
+				writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+				for obj in list_objs:
+					writer.writerow(obj.to_dictionary())
+
+	@classmethod
+	def load_from_file_csv(cls):
+		"""Deserializes CSV format from a file"""
+		filename = cls.__name__+ ".csv"
+		try:
+			with open(filename, "r", newline="") as csvfile:
+				if cls.__name__ == "Rectangle":
+					fieldnames = ["id", "width", "height", "x", "y"]
+				else:
+					fieldnames = ["id", "size", "x", "y"]
+			list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+			list_dicts = [dict([k, int(v)] for k, v in d.items())
+					for d in list_dicts]
+			return [cls.create(**d) for d in list_dicts]
+		except IOError:
+			return []
+
+	@staticmethod
+	def draw(list_rectangles, list_squares):
+		"""Draw Rectangles and Squares using the turtle module.
+		Args:
+			list_rectangles (list_; A list of Rectangle objects to draw.
+			list_squares (list): A list of square objects to draw.
+		"""
+		turt = turtle.Turtle()
+		turt.screen.bgcolor("#b7312c")
+		turt.pensize(3)
+		turt.shape("turtle")
+
+		turt.color("#ffffff")
+		for rect in list_rectangles:
+			turt.showturtle()
+			turt.up()
+			turt.goto(rect.x, rect.y)
+			turt.down()
+			for i in range(2):
+				turt.forward(rect.width)
+				turt.left(90)
+				turt.forward(rect.height)
+				turt.left(90)
+			turt.hideturtle()
+
+		turt.color("#b5e3d8")
+		for sq in list_squares:
+			turt.showturtle()
+			turt.up()
+			turt.goto(sq.x, sq.y)
+			turt.down()
+			for i in range(2):
+				turt.forward(sq.width
+				turt.left(90)
+				turt.forward(sq.height)
+				turt.left(90)
+			turt.hideturtle()
+
+		turtle.exitonclick()
