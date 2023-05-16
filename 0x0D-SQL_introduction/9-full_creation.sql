fe69ab500d68deb5e3d0d93c911692abae1cf665
Abijahebe <Abijahebe911@gmail.com>
2023-05-16 16:19:39 -0700

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR(256),
 	score INT
-):
+);
 
 INSERT INTO second_table
 (id, name, score)
@@ -11,7 +11,7 @@ VALUES(1, "John", 10);
 
 INSERT INTO second_table
 (id, name, score)
-VALUES(2, "Alex", 3):
+VALUES(2, "Alex", 3);
 
 INSERT INTO  second_table
 (id, name, score)
