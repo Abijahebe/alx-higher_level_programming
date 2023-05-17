bce52c0dfc1748cd3374e9e1412f4fa96dd1693f
Abijahebe <Abijahebe911@gmail.com>
2023-05-17 04:32:12 -0700

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates the MySQL server user_0d_1
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
