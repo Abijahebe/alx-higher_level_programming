e4471c2c66c07429615d60794491bb7e06d28dff
Abijahebe <Abijahebe911@gmail.com>
2023-05-17 05:45:26 -0700

    tasks 10-16

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- lists all shows without genre in the db
+
+SELECT ts.title, tsg.genre_id
+FROM tv_shows AS ts
+	LEFT OUTER JOIN tv_show_genres AS tsg
+	ON ts.id = tsg.show_id
+WHERE tsg.show_id IS NULL
+ORDER BY ts.title, tsg.genre_id;
---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- lists all genres of a showw inthe db
+
+SELECT tg.name
+FROM tv_shows AS ts
+	INNER JOIN tv_shows_genres AS tsg
+		ON ts.id = tsg.show_id
+	INNER JOIN tv_genres AS tg
+		ON tg.id = tsg.genre_id
+where ts.title = "Dexter"
+ORDER BY tg.name;
