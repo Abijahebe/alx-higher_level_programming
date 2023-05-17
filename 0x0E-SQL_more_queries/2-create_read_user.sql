8a1a82c06386dadb09b4042d0d7782eddde4742c
Abijahebe <Abijahebe911@gmail.com>
2023-05-17 04:56:58 -0700

    tasks 3-5

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
---creates the database hbtn_0d_2 and the user user_0d_2
+-- creates the database hbtn_0d_2 and the user user_0d_2
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 DROP USER IF EXISTS 'user_0d_2'@'localhost';
