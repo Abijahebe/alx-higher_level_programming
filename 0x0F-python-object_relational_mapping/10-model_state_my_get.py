df7c1c9810ae20ec43442667405c7dcfbf5f53ec
Abijahebe <Abijahebe911@gmail.com>
2023-06-21 11:22:36 -0700

    task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,7 +8,7 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name == '__main__':
+if __name__ == '__main__':
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
             argv[1], argv[2], argv[3])
     engine = create_engine(db_url)
