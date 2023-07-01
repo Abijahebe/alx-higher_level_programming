99446baf683df3ba1291feda5bc79a38cc83ff6e
Abijahebe <Abijahebe911@gmail.com>
2023-07-01 06:09:31 -0700

    task 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-curl -s -o response.txt 0.0.0.0:5000/catch_me && grep -o "You got me!" response.txt>&2
+# Make a request to the server and save the response in a file
+response_file=$(mktemp)
+curl -s -o "$response_file" "0.0.0.0:5000/catch_me"
+
+# Extract the message from the ressponse file
+message=$(grep -o "You got me!" "$response_file")
+
+# Display the message without using echo or cat
+exec 1>&2
+printf '%s\n' "$message"
